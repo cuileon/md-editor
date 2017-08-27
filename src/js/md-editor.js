8c4e4358775530bb171e85fd581172aa445351c7
cuileon <cuiliang@live.cn>
2017-08-27 15:56:23 +0800

    add setAssemble function

--- src/js/md-editor.js
+++ src/js/md-editor.js
@@ -1,147 +1,154 @@
 (function ($) {
     'use strict';
     var MarkdownEditor = function (ele, opt) {
-        this.$element = ele,
-            this.defaults = {
-                debug: false,
-                codeMirror: {
-                    tabSize: '4',
-                    indentWithTabs: true,
-                    lineWrapping: true,
-                    extraKeys: {
-                        "Enter": 'newlineAndIndentContinueMarkdownList',
-                    },
-                    mode: 'markdown',
-                },
-                buttons: [
-                    {
-                        name: "mainHeader",
-                        className: "fa fa-header",
-                        action: function () {
-                            this.insertBefore('## ', 3);
-                        }
-                    },
-                    {
-                        name: "subHeader",
-                        className: "fa fa-header",
-                        action: function () {
-                            this.insertBefore('### ', 4);
-                        }
-                    },
-                    {
-                        name: "bold",
-                        className: "fa fa-bold",
-                        action: function () {
-                            this.insertAround('**', '**')
-                        }
-                    },
-                    {
-                        name: "italicize",
-                        className: "fa fa-italic",
-                        action: function () {
-                            this.insertAround('*', '*');
-                        }
-                    },
-                    {
-                        name: "blockquote",
-                        className: "fa fa-quote-left",
-                        action: function () {
-                            this.insertBefore('> ', 2);
-                        }
-                    },
-                    {
-                        name: "link",
-                        className: "fa fa-link",
-                        action: function () {
-                            this.insertAround('[', '](http://)');
-                        }
-                    },
-                    {
-                        name: "image",
-                        className: "fa fa-image",
-                        action: function () {
-                            this.insertBefore('![](http://)', 2);
-                        }
-                    },
-                    {
-                        name: "unorderedList",
-                        className: "fa fa-list",
-                        action: function () {
-                            this.insertBefore('* ', 2);
-                        }
-
-                    },
-                    {
-                        name: "orderedList",
-                        className: "fa fa-list-ol",
-                        action: function () {
-                            this.insertBefore('1. ', 3);
-                        }
-                    },
-                    {
-                        name: "code",
-                        className: "fa fa-code",
-                        action: function () {
-                            if (this.cm.getDoc().getSelection().indexOf('\n') > 0) {
-                                this.insertAround('```\r\n', '\r\n```');
-                            } else {
-                                this.insertAround('`', '`');
-                            }
-                        }
-                    },
-                    {
-                        name: "preview",
-                        className: "fa fa-search",
-                        action: function () {
-                            if ($(".md-editor-preview").length > 0) {
-                                $('.CodeMirror-code').show();
-                                $(".md-editor-toolbar li a").removeClass("disabled");
-                                $('.md-editor-preview').remove();
-                            } else {
-                                $('.CodeMirror-code').hide();
-                                $(".md-editor-toolbar li:not(.preview,.fullScreen) a").addClass("disabled");
-                                $('<div/>').addClass('md-editor-preview').html(marked(this.cm.getValue())).insertAfter($('.CodeMirror-code'));
-                            }
-                        }
-                    },
-                    {
-                        name: "fullScreen",
-                        className: "fa fa-expand",
-                        action: function () {
-                            var el = this.cm.getWrapperElement();
-                            if ($(el.parentNode).hasClass('fullscreen')) {
-                                $('.fullScreen a').removeClass('fa-compress').addClass('fa-expand');
-                                $(el.parentNode).removeClass('fullscreen');
-                                $(el).height('300');
-                            } else {
-                                $(el).height('100%');
-                                $('.fullScreen a').removeClass('fa-expand').addClass('fa-compress');
-                                $(el.parentNode).addClass('fullscreen');
-                            }
+        this.$element = ele;
+        this.defaults = {
+            debug: false,
+            codeMirror: {
+                lineWrapping: true,
+                extraKeys: {
+                    Enter: 'newlineAndIndentContinueMarkdownList',
+                    Tab: function(cm) {
+                        if (cm.somethingSelected()) {
+                            cm.indentSelection("add");
+                        } else {
+                            cm.replaceSelection(cm.getOption("indentWithTabs") ? "\t" : Array(cm.getOption("indentUnit") + 1).join(" "), "end", "+input");
                         }
                     }
-                ],
-                additionalButtons: [],
-                disabledButtons: [],
+                },
+                mode: 'markdown',
             },
-            this.options = $.extend({}, this.defaults, opt);
-    }
+            buttons: [
+                {
+                    name: "mainHeader",
+                    className: "fa fa-header",
+                    action: function () {
+                        this.insertBefore('## ', 3);
+                    }
+                },
+                {
+                    name: "subHeader",
+                    className: "fa fa-header",
+                    action: function () {
+                        this.insertBefore('### ', 4);
+                    }
+                },
+                {
+                    name: "bold",
+                    className: "fa fa-bold",
+                    action: function () {
+                        this.insertAround('**', '**')
+                    }
+                },
+                {
+                    name: "italicize",
+                    className: "fa fa-italic",
+                    action: function () {
+                        this.insertAround('*', '*');
+                    }
+                },
+                {
+                    name: "blockquote",
+                    className: "fa fa-quote-left",
+                    action: function () {
+                        this.insertBefore('> ', 2);
+                    }
+                },
+                {
+                    name: "link",
+                    className: "fa fa-link",
+                    action: function () {
+                        this.insertAround('[', '](http://)');
+                    }
+                },
+                {
+                    name: "image",
+                    className: "fa fa-image",
+                    action: function () {
+                        this.insertBefore('![](http://)', 2);
+                    }
+                },
+                {
+                    name: "unorderedList",
+                    className: "fa fa-list",
+                    action: function () {
+                        this.insertBefore('* ', 2);
+                    }
+
+                },
+                {
+                    name: "orderedList",
+                    className: "fa fa-list-ol",
+                    action: function () {
+                        this.insertBefore('1. ', 3);
+                    }
+                },
+                {
+                    name: "code",
+                    className: "fa fa-code",
+                    action: function () {
+                        if (this.cm.getDoc().getSelection().indexOf('\n') > 0) {
+                            this.insertAround('```\r\n', '\r\n```');
+                        } else {
+                            this.insertAround('`', '`');
+                        }
+                    }
+                },
+                {
+                    name: "preview",
+                    className: "fa fa-search",
+                    action: function () {
+                        if ($(".md-editor-preview").length > 0) {
+                            $('.CodeMirror-code').show();
+                            $(".md-editor-toolbar li a").removeClass("disabled");
+                            $('.md-editor-preview').remove();
+                        } else {
+                            $('.CodeMirror-code').hide();
+                            $(".md-editor-toolbar li:not(.preview,.fullScreen) a").addClass("disabled");
+                            $('<div/>').addClass('md-editor-preview').html(marked(this.cm.getValue())).insertAfter($('.CodeMirror-code'));
+                        }
+                    }
+                },
+                {
+                    name: "fullScreen",
+                    className: "fa fa-expand",
+                    action: function () {
+                        var el = this.cm.getWrapperElement();
+                        if ($(el.parentNode).hasClass('fullscreen')) {
+                            $('.fullScreen a').removeClass('fa-compress').addClass('fa-expand');
+                            $(el.parentNode).removeClass('fullscreen');
+                            $(el).height('300');
+                        } else {
+                            $(el).height('100%');
+                            $('.fullScreen a').removeClass('fa-expand').addClass('fa-compress');
+                            $(el.parentNode).addClass('fullscreen');
+                        }
+                    }
+                }
+            ],
+            additionalButtons: [],
+            disabledButtons: []
+        };
+        this.options = $.extend({}, this.defaults, opt);
+    };
 
     MarkdownEditor.prototype = {
         debug: function () {
         },
+        setWrapper: function () {
+            this.wrapper = $('<div/>').addClass('md-editor');
+        },
         setCodeMirror: function () {
             this.cm = CodeMirror.fromTextArea(this.$element[0], this.options.codeMirror);
         },
-        setToolbar: function (options) {
+        setToolbar: function () {
             var editor = this;
             var cm = editor.cm;
-            var cmWrapper = cm.getWrapperElement();
-            var toolbar = $('<ul/>').addClass('md-editor-toolbar').insertBefore($(cmWrapper));
+            var toolbar = $('<ul/>').addClass('md-editor-toolbar');
             this.options.buttons.map(function (button) {
                 var item = $('<li/>').addClass(button.name),
                     anchor = $('<a/>').addClass(button.className);
-                anchor.appendTo(item).click(function (e) {
+                anchor.appendTo(item).click(function () {
                     if (button.action && !$(this).hasClass('disabled')) {
                         cm.focus();
                         button.action.call(editor);
@@ -149,6 +156,12 @@
                 });
                 toolbar.append(item);
             });
+            this.toolbar = toolbar;
+        },
+        setAssemble: function () {
+            var cmWrapper = this.cm.getWrapperElement();
+            $(cmWrapper).wrap(this.wrapper);
+            this.toolbar.insertBefore(cmWrapper);
         },
         insert: function insert(insertion) {
             var doc = this.cm.getDoc();
@@ -192,8 +205,10 @@
 
     $.fn.mdEditor = function (options) {
         var markdownEditor = new MarkdownEditor(this, options);
+        markdownEditor.setWrapper();
         markdownEditor.setCodeMirror();
         markdownEditor.setToolbar();
+        markdownEditor.setAssemble();
         return this;
     };
 })(window.jQuery);
